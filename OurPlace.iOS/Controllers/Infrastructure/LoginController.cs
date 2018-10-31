707d57ba2110a89463a5146bc713e61324dd9cba
Dan Richardson <d.richardson@ncl.ac.uk>
2018-10-31 14:59:37 +0000

    Removed dependency on Rivets

--- OurPlace.iOS/Controllers/Infrastructure/LoginController.cs
+++ OurPlace.iOS/Controllers/Infrastructure/LoginController.cs
@@ -26,34 +26,33 @@ using OurPlace.Common;
 using OurPlace.Common.Models;
 using OurPlace.iOS.Helpers;
 using SafariServices;
-using static OurPlace.Common.LocalData.Storage;
-using Rivets;
 using System.Threading.Tasks;
 using CoreGraphics;
 using System.Linq;
+using OurPlace.Common.LocalData;
 
 namespace OurPlace.iOS
 {
     public partial class LoginController : UIViewController
     {
-		private ExternalLogin[] providers;
+        private ExternalLogin[] providers;
         private LoadingOverlay loadPop;
         private NSObject notificationToken;
         private SFSafariViewController safariController;
-        
-		private string accessToken;
+
+        private string accessToken;
         private DateTime accessExpiresAt;
         private string refreshToken;
         private DateTime refreshExpiresAt;
-        
-        public LoginController(IntPtr handle) : base (handle)
-        {
-            
-		}
 
-		public override async void ViewDidLoad()
-		{
-			base.ViewDidLoad();
+        public LoginController(IntPtr handle) : base(handle)
+        {
+
+        }
+
+        public override async void ViewDidLoad()
+        {
+            base.ViewDidLoad();
 
             if (!AppDelegate.IsInDesignerView)
             {
@@ -61,30 +60,30 @@ namespace OurPlace.iOS
             }
 
             InfoButton.TouchUpInside += InfoButton_TouchUpInside;
-		}
+        }
 
-		public override void ViewWillAppear(bool animated)
-		{
+        public override void ViewWillAppear(bool animated)
+        {
             base.ViewWillAppear(animated);
 
-			NavigationController.SetNavigationBarHidden(true, true);
-		}
+            NavigationController.SetNavigationBarHidden(true, true);
+        }
 
-		private async Task GetExternalLoginProviders()
+        private async Task GetExternalLoginProviders()
         {
-			var bounds = UIScreen.MainScreen.Bounds;
+            var bounds = UIScreen.MainScreen.Bounds;
 
-			// show the loading overlay on the UI thread using the correct orientation sizing
-			loadPop = new LoadingOverlay(bounds);
-			View.Add(loadPop);
+            // show the loading overlay on the UI thread using the correct orientation sizing
+            loadPop = new LoadingOverlay(bounds);
+            View.Add(loadPop);
 
-			string fullUrl = "/api/Account/ExternalLogins?returnUrl=/callback&generateState=true";
-			ServerResponse<ExternalLogin[]> res = await ServerUtils.Get<ExternalLogin[]>(fullUrl, false);
+            string fullUrl = "/api/Account/ExternalLogins?returnUrl=/callback&generateState=true";
+            ServerResponse<ExternalLogin[]> res = await ServerUtils.Get<ExternalLogin[]>(fullUrl, false);
 
             loadPop.Hide();
 
-			if (res.Success)
-			{
+            if (res.Success)
+            {
                 providers = res.Data;
 
                 for (int i = 0; i < providers.Length; i++)
@@ -114,33 +113,36 @@ namespace OurPlace.iOS
 
                     PromptLabel.Superview.AddSubview(button);
                 }
-			}
-			else
-			{
-				// Show error and retry button
+            }
+            else
+            {
+                // Show error and retry button
                 var errorAlert = UIAlertController.Create("Connection Error", "Couldn't load login info from the server. Please try again.", UIAlertControllerStyle.Alert);
-                errorAlert.AddAction(UIAlertAction.Create("Retry", UIAlertActionStyle.Default, (a)=>{ var suppressAsync = GetExternalLoginProviders(); }));
-				PresentViewController(errorAlert, true, null);
-			}
-		}
+                errorAlert.AddAction(UIAlertAction.Create("Retry", UIAlertActionStyle.Default, (a) => { var suppressAsync = GetExternalLoginProviders(); }));
+                PresentViewController(errorAlert, true, null);
+            }
+        }
 
         private void LoginAfterTerms(string loginUri)
         {
-            var alertController = UIAlertController.Create("Terms & Privacy Policy", 
+            var alertController = UIAlertController.Create("Terms & Privacy Policy",
                                                            "By continuing you confirm that you have read and agree to OurPlace's Terms & Conditions and Privacy Policy",
                                                            UIAlertControllerStyle.Alert);
-            
-            alertController.AddAction(UIAlertAction.Create("Read Terms & Conditions", UIAlertActionStyle.Default, (obj) => {
+
+            alertController.AddAction(UIAlertAction.Create("Read Terms & Conditions", UIAlertActionStyle.Default, (obj) =>
+            {
                 UIApplication.SharedApplication.OpenUrl(new NSUrl(ConfidentialData.api + "terms"));
             }));
 
-            alertController.AddAction(UIAlertAction.Create("Read Privacy Policy", UIAlertActionStyle.Default, (obj) => {
+            alertController.AddAction(UIAlertAction.Create("Read Privacy Policy", UIAlertActionStyle.Default, (obj) =>
+            {
                 UIApplication.SharedApplication.OpenUrl(new NSUrl(ConfidentialData.api + "privacypolicy"));
             }));
 
-            alertController.AddAction(UIAlertAction.Create("Cancel", UIAlertActionStyle.Cancel, (obj) => {}));
+            alertController.AddAction(UIAlertAction.Create("Cancel", UIAlertActionStyle.Cancel, (obj) => { }));
 
-            alertController.AddAction(UIAlertAction.Create("Agree", UIAlertActionStyle.Default, (obj) => {
+            alertController.AddAction(UIAlertAction.Create("Agree", UIAlertActionStyle.Default, (obj) =>
+            {
                 safariController = new SFSafariViewController(new NSUrl(ConfidentialData.api + loginUri));
                 PresentViewControllerAsync(safariController, true);
             }));
@@ -154,7 +156,8 @@ namespace OurPlace.iOS
                 "SignInExplanationController") as SignInExplanationController;
             var navController = new UINavigationController(explanationController);
 
-            explanationController.NavigationItem.LeftBarButtonItem = new UIBarButtonItem("Close", UIBarButtonItemStyle.Plain, delegate {
+            explanationController.NavigationItem.LeftBarButtonItem = new UIBarButtonItem("Close", UIBarButtonItemStyle.Plain, delegate
+            {
                 explanationController.DismissViewController(true, null);
             });
 
@@ -167,41 +170,49 @@ namespace OurPlace.iOS
         {
             try
             {
-				var bounds = UIScreen.MainScreen.Bounds;
-				loadPop = new LoadingOverlay(bounds);
-				View.Add(loadPop);
+                var bounds = UIScreen.MainScreen.Bounds;
+                loadPop = new LoadingOverlay(bounds);
+                View.Add(loadPop);
 
-				// Login return format:
-				// parklearn://logincallback?access_token=TOKEN&token_type=bearer&expires_in=7257600&state=STATE"
+                // Login return format:
+                // parklearn://logincallback?access_token=TOKEN&token_type=bearer&expires_in=7257600&state=STATE"
 
-                AppLinkUrl appLinkUrl = new AppLinkUrl(notification.UserInfo[new NSString("url")].ToString());
+                NSObject returned = notification.UserInfo?[new NSString("url")];
+                if (returned == null)
+                {
+                    throw new Exception("Error retrieving data from server");
+                }
 
                 await safariController.DismissViewControllerAsync(true);
                 NSNotificationCenter.DefaultCenter.RemoveObserver(notificationToken);
 
-				if (appLinkUrl.InputQueryParameters.ContainsKey("access_token"))
-				{
-					accessToken = appLinkUrl.InputQueryParameters["access_token"];
-				}
-				else { throw new Exception("No access token returned"); }
+                Uri uri = new Uri(returned.ToString());
 
-                if (appLinkUrl.InputQueryParameters.ContainsKey("expires_in"))
+                accessToken = Common.Helpers.GetUrlParam(uri, "access_token");
+                if (string.IsNullOrWhiteSpace(accessToken))
                 {
-                    accessExpiresAt = DateTime.UtcNow.AddSeconds(int.Parse(appLinkUrl.InputQueryParameters["expires_in"]));
+                    throw new Exception("No access token returned");
                 }
-                else { throw new Exception("No access token expiry information"); }
 
-                if (appLinkUrl.InputQueryParameters.ContainsKey("refresh_token"))
+                refreshToken = Common.Helpers.GetUrlParam(uri, "refresh_token");
+                if (string.IsNullOrWhiteSpace(refreshToken))
                 {
-                    refreshToken = appLinkUrl.InputQueryParameters["refresh_token"];
+                    throw new Exception("No refresh token returned");
                 }
-                else { throw new Exception("No refresh token returned"); }
 
-                if (appLinkUrl.InputQueryParameters.ContainsKey("refresh_token_expires"))
+                string expiresInSecs = Common.Helpers.GetUrlParam(uri, "expires_in");
+                if (string.IsNullOrWhiteSpace(expiresInSecs))
                 {
-                    refreshExpiresAt = new DateTime(long.Parse(appLinkUrl.InputQueryParameters["refresh_token_expires"]), DateTimeKind.Utc);
+                    throw new Exception("No access token expiry information");
                 }
-                else { throw new Exception("No refresh token expiry information"); }
+                accessExpiresAt = DateTime.UtcNow.AddSeconds(int.Parse(expiresInSecs));
+
+                string refreshExpiry = Common.Helpers.GetUrlParam(uri, "refresh_token_expires");
+                if (string.IsNullOrWhiteSpace(refreshExpiry))
+                {
+                    throw new Exception("No refresh token expiry information");
+                }
+                refreshExpiresAt = new DateTime(long.Parse(refreshExpiry), DateTimeKind.Utc);
 
                 ApplicationUser tempUser = new ApplicationUser()
                 {
@@ -211,45 +222,45 @@ namespace OurPlace.iOS
                     RefreshExpiresAt = refreshExpiresAt
                 };
 
-                (await GetDatabaseManager()).AddUser(tempUser);
+                (await Storage.GetDatabaseManager()).AddUser(tempUser);
 
-				ServerResponse<ApplicationUser> res = await ServerUtils.Get<ApplicationUser>("api/account/");
+                ServerResponse<ApplicationUser> res = await ServerUtils.Get<ApplicationUser>("api/account/");
 
-				if (res != null && res.Success)
-				{
+                if (res != null && res.Success)
+                {
                     ApplicationUser updatedUser = res.Data;
                     updatedUser.AccessToken = tempUser.AccessToken;
                     updatedUser.AccessExpiresAt = tempUser.AccessExpiresAt;
                     updatedUser.RefreshToken = tempUser.RefreshToken;
                     updatedUser.RefreshExpiresAt = tempUser.RefreshExpiresAt;
 
-                    (await GetDatabaseManager()).AddUser(updatedUser);
+                    (await Storage.GetDatabaseManager()).AddUser(updatedUser);
 
                     loadPop.Hide();
 
-					NavigationController.SetNavigationBarHidden(false, true);
+                    NavigationController.SetNavigationBarHidden(false, true);
 
                     var storyBoard = UIStoryboard.FromName("Main", null);
                     var mainScreen = storyBoard.InstantiateViewController("MainTabController");
 
                     UIApplication.SharedApplication.KeyWindow.RootViewController = new UINavigationController(mainScreen);
                     NavigationController.PopToRootViewController(true);
-			    }
-				else
-				{
+                }
+                else
+                {
                     throw new Exception("Login failed");
-				}
+                }
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 loadPop.Hide();
 
                 var errorAlert = UIAlertController.Create("Login Error", e.Message, UIAlertControllerStyle.Alert);
-				PresentViewController(errorAlert, true, null);
+                PresentViewController(errorAlert, true, null);
 
                 Console.WriteLine(e.Message);
             }
-		}
+        }
 
     }
 }
